175f5d8530175c760c289e3671098c0adae73f2f
Gayushri <121676999+Gayushri@users.noreply.github.com>
2023-02-03 12:39:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,7 +44,6 @@ resource "aws_internet_gateway" "web-gw" {
     Name = "web"
   }
 }
-
 #creating routing table
 resource "aws_route_table" "web-public1" {
   vpc_id = aws_vpc.web.id
@@ -58,7 +57,7 @@ resource "aws_route_table" "web-public1" {
   }
 }
 
-#creating routing table 
+#creating routing table
 resource "aws_route_table" "web-private1" {
   vpc_id = aws_vpc.web.id
  route {
@@ -84,17 +83,89 @@ resource "aws_route_table_association" "web-private1" {
   route_table_id = aws_route_table.web-private1.id
 }
 
+
+resource "aws_security_group" "allow_tls" {
+
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.web.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+ #  ipv4_cidr_blocks = [aws_vpc.web.ipv4_cidr_block]
+    }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+#   ipv4_cidr_blocks = ["::/0"]
+   }
+
+}
+
 #creating EC2
 resource "aws_instance" "server1" {
   ami                           = "ami-0be0a52ed3f231c12"
   instance_type                 = "t2.micro"
   subnet_id                     = aws_subnet.web-public1.id
+#  security_groups              = [ "allow_tls" ]
+  vpc_security_group_ids        = [aws_security_group.allow_tls.id]
   associate_public_ip_address   = true
   key_name                      = "Pkey"
 
   tags = {
     Name = "server1"
   }
-}
 
-      
+  connection {
+    host = aws_instance.server1.public_ip
+    type = "ssh"
+    user = "ubuntu"
+    private_key = file("ansible.pem")
+
+  }
+
+  provisioner "local-exec" {
+     command = "echo ${self.public_ip} > public_ips.txt"
+  }
+
+  provisioner "local-exec" {
+
+     command = "ansible-playbook play.yml -i public_ips.txt"
+  }
+
+}
+#
+#
+##IP of aws instance retrieved
+#output "instance_id" {
+#  description = "ID of the EC2 instance"
+#  value       = aws_instance.server1.id
+#}
+#
+#output "instance_public_ip" {
+#  description = "Public IP address of the EC2 instance"
+#  value       = aws_instance.server1.public_ip
+#
+#}
+
+
+
+
+
+#resource "null_resource" "cluster" {
+#
+#  # So we just choose the first in this case
+#  connection {
+#    host = aws_instance.server1.public_ip
+#    type = "ssh"
+#    private_key = file("ansible.pem")
+#
+#  }
+                                   
