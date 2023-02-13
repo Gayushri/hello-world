aa25edbdc9e7ba9087d43362c887e0618f2a02c1
Gayushri <121676999+Gayushri@users.noreply.github.com>
2023-02-13 13:02:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -130,18 +130,19 @@ resource "aws_instance" "server1" {
   }
 
   provisioner "local-exec" {
-    command = "echo ${self.public_ip} > /etc/ansible/hosts"
-    interpreter = ["/bin/bash", "-c"]
+    #command = "echo ${self.public_ip} > /etc/ansible/hosts"
+    #interpreter = ["/bin/bash", "-c"]
    
-    #command = <<EOT
-      #echo ${self.public_ip} > /etc/ansible/hosts
-      #echo -e '${self.public_ip} \t ansible_ssh_private_key_file=/var/lib/jenkins/workspace/dynamic-job/ansible.pem' >> /etc/ansible/hosts
-    #EOT
+    command = <<-EOT
+      echo ${self.public_ip} > /etc/ansible/hosts  
+      echo ${self.public_ip} \t ansible_ssh_private_key_file=/var/lib/jenkins/workspace/dynamic-job/ansible.pem >> /etc/ansible/hosts
+      interpreter = ["/bin/bash", "-c"]
+    EOT
     
 }
-  provisioner "local-exec" {
-    command = "echo ${self.public_ip} ansible_ssh_private_key_file=/var/lib/jenkins/workspace/dynamic-job/ansible.pem >> /etc/ansible/hosts"
-    interpreter = ["/bin/bash", "-c"]
+  #provisioner "local-exec" {
+    #command = "echo ${self.public_ip} ansible_ssh_private_key_file=/var/lib/jenkins/workspace/dynamic-job/ansible.pem >> /etc/ansible/hosts"
+    #interpreter = ["/bin/bash", "-c"]
 }
 
   
