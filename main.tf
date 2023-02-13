b6b64b977d739c0dc5d44917ae3fa53b7ecf364e
Gayushri <121676999+Gayushri@users.noreply.github.com>
2023-02-13 11:23:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -134,7 +134,7 @@ resource "aws_instance" "server1" {
     #"./${self.public_ip} >> ansible_ssh_private_key_file=/var/lib/jenkins/workspace/dynamic-job/ansible.pem"
     command = <<EOT
       "echo ${self.public_ip} > /etc/ansible/hosts"
-      "./${self.public_ip} >> ansible_ssh_private_key_file=/var/lib/jenkins/workspace/dynamic-job/ansible.pem"
+      "echo ${self.public_ip} ansible_ssh_private_key_file=/var/lib/jenkins/workspace/dynamic-job/ansible.pem >> /etc/ansible/hosts"
     EOT
     
 }
